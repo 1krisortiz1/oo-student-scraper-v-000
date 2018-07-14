529c265e59dc4156d4b99137c8a29a4fd13cca32
Kristin Ortiz <itskristin.ortiz@gmail.com>
2018-07-14 02:42:03 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -8,8 +8,11 @@ class Student
     @@all << self
   end
 
+  def new
+  end
+
   def self.create_from_collection(students_array)
-    Student.new = student_hash
+    
   end
 
   def add_student_attributes(attributes_hash)
