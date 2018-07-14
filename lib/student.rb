ae8f5d15ba11f1f2885895ef5a0c7710410efefb
Kristin Ortiz <itskristin.ortiz@gmail.com>
2018-07-14 04:32:40 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -8,17 +8,17 @@ class Student
     student_hash.each {|key, value| self.send(("#{key}="), value)}
     #takes in an argument of a hash and sets that new student's attributes using th
     #key/value pairs of that hash.
-    @@all << self 
+    @@all << self
     #adds that new student to the Student class' collection of all existing students,
     #stored in the '@@all' class variable.
   end
 
   def self.create_from_collection(students_array)
-
+    students_array.each { |student| self.new(student)}
   end
 
   def add_student_attributes(attributes_hash)
-
+    attribut
   end
 
   def self.all
